ed1c9cb0f104b389da8e61d8dc922d90b633f464
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-12-02 18:35:46 -0300

    chore: Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -10,6 +10,25 @@ export function Summary() {
   const { transactions } = useContext(TransactionsContext)
   const theme = useTheme()
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -18,7 +37,7 @@ export function Summary() {
 
           <ArrowCircleUp size={30} color={theme['green-300']} />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -28,7 +47,7 @@ export function Summary() {
           <ArrowCircleDown size={30} color={theme['red-300']} />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -37,7 +56,7 @@ export function Summary() {
 
           <CurrencyDollar size={30} color={theme.white} />
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
