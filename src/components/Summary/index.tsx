e9bbd11163f2d996db2e3348107fdb10f12c6b1a
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2023-12-04 20:50:04 -0300

    feat: Criando hook para o resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,34 +1,14 @@
 import { useTheme } from 'styled-components'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
-import { useContext } from 'react'
 
-import { TransactionsContext } from '../../context/TransactionsContext'
+import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 import { SummaryCard, SummaryContainer } from './styles'
-import { priceFormatter } from '../../utils/formatter'
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext)
+  const summary = useSummary()
   const theme = useTheme()
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
 
   return (
     <SummaryContainer>
